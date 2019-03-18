34dcc02cfb3451cce3b59580de2ea43717e4e31c
Nick Turner <NTurner@ufcunet.ad>
2019-03-18 09:50:36 -0500

    initial commit

---
+++ EFGetStarted.AspNet.Core.NewDb/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace EFGetStarted.AspNet.Core.NewDb.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# EFGetStarted.AspNet.Core.NewDb
